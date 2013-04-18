9475f0a235ed631908ae7dc80e5d6749703b7073
Sergey Alpeev <sergey.alpeev@gmail.com>
2013-04-18 13:47:37 +0300

    Initial Commit

---
+++ LearnGL3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearnGL3
+//
+//  Created by Sergey Alpeev on 4/18/13.
+//  Copyright (c) 2013 Sergey Alpeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LearnGL3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LearnGL3
+//
+//  Created by Sergey Alpeev on 4/18/13.
+//  Copyright (c) 2013 Sergey Alpeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
